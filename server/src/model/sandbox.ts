4fe76a9b3743844230d66608a7b7c7b6aef60179
pradeepkundekar <pradeepkundekargithub@gmail.com>
2024-01-15 14:24:54 +0530

    Added model for sandbox

---
+++ server/src/model/sandbox.ts
@@ -0,0 +1,24 @@
+import mongoose, { ObjectId } from "mongoose";
+interface ISandBox{
+    code:string
+    output:string
+    userId: ObjectId
+}
+const SandBoxSchema = new mongoose.Schema<ISandBox>({
+    code:{
+        type:String,
+        min:3,
+        required:true
+    },
+    output:{
+        type:String,
+        min:3,
+        required:true
+    },
+    userId:{
+        type:mongoose.Types.ObjectId,
+        ref:"User",
+        required:[true,"User id is required"]
+    }
+});
+export const sandBox = mongoose.model("SandBox",SandBoxSchema);
