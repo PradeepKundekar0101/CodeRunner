4b865253031ae084053141789ca425ba4d3f70d5
Pradeep Kundekar <91021094+PradeepKundekar0101@users.noreply.github.com>
2024-01-18 13:26:09 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import { app } from './app';
 import dotenv from 'dotenv'
 import {connectDB} from './db'
 dotenv.config();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 connectDB().then(()=>{
     app.listen(PORT,()=>{
         console.log("Server running at PORT "+PORT);
