9658fef4cfd4ce6c796b54992118c5cdb7f36ae6
pradeepkundekar <pradeepkundekargithub@gmail.com>
2024-01-20 17:14:28 +0530

    Added rate limit for code execute

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,22 @@ import cors from 'cors';
 import express from 'express';
 import userRoute from './routes/user.routes'
 import codeRoute from './routes/code.routes'
+import {rateLimit} from 'express-rate-limit'
 const app = express();
 app.use(cors({origin: "*" }));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(express.json());
+
+// Define the rate limiter middleware
+const limiter = rateLimit({
+  windowMs: 10 * 1000, 
+  max: 1, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again later.',
+});
+
+app.use('/api/v1/code', limiter);
+
 app.use('/api/v1/user',userRoute);
 app.use('/api/v1/code',codeRoute)
 // Unhandled Routes:
