5a17374e80af444410e8fefd745206e95c61cf46
pradeepkundekar <pradeepkundekargithub@gmail.com>
2024-01-23 22:44:21 +0530

    Added BullMQ

--- server/build/controller/code.js
+++ server/build/controller/code.js
@@ -15,70 +15,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.status = exports.saveCode = exports.executeCode = void 0;
 const apiError_1 = require("../utils/apiError");
 const apiResponse_1 = require("../utils/apiResponse");
-const dockerode_1 = __importDefault(require("dockerode"));
 const asyncHandler_1 = require("../utils/asyncHandler");
 const job_1 = __importDefault(require("../model/job"));
+const bullmq_1 = require("bullmq");
+const jobQueue = new bullmq_1.Queue("jobQueue", {
+    connection: {
+        host: "0.0.0.0",
+        port: 6379
+    }
+});
 exports.executeCode = (0, asyncHandler_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const docker = new dockerode_1.default();
     const { code, userId, language } = req.body;
     if (!code || !language) {
         throw new apiError_1.ApiError(400, "Code and language are required");
     }
     const job = yield job_1.default.create({ code, language, userId });
     res.status(200).json({ jobId: job._id });
-    let image;
-    let command;
-    // Define Docker configurations based on language
-    switch (language.toLowerCase()) {
-        case 'javascript':
-            image = 'node';
-            command = ['node', '-e', code];
-            break;
-        case 'java':
-            image = 'openjdk';
-            command = ['bash', '-c', `echo '${code}' > Main.java && javac Main.java && java Main`];
-            break;
-        case 'cpp':
-            image = 'gcc';
-            command = ['bash', '-c', `echo '${code}' > main.cpp && g++ main.cpp -o main && ./main`];
-            break;
-        case 'python':
-            image = 'python:latest';
-            command = ['bash', '-c', `echo '${code}' > script.py && python script.py`];
-            break;
-        case 'c':
-            image = 'gcc';
-            command = ['bash', '-c', `echo '${code}' > main.c && gcc main.c -o main && ./main`];
-            break;
-        default:
-            throw new apiError_1.ApiError(400, "Unsupported language");
-    }
-    const containerConfig = {
-        Image: image,
-        Tty: false,
-        AttachStdout: true,
-        AttachStderr: true,
-        Cmd: command,
-    };
-    try {
-        job.startedAt = new Date();
-        const container = yield docker.createContainer(containerConfig);
-        yield container.start();
-        yield container.wait();
-        const containerLogs = yield container.logs({ stdout: true, stderr: true });
-        const containerResult = containerLogs.toString('utf-8').trim();
-        console.log(containerResult);
-        job["completedAt"] = new Date();
-        job["status"] = "success";
-        job["output"] = containerResult;
-        yield job.save();
-    }
-    catch (error) {
-        job["completedAt"] = new Date();
-        job["output"] = error.message;
-        job["status"] = "failed";
-        throw new apiError_1.ApiError(500, JSON.stringify(error.message));
-    }
+    console.log("Adding job in queue");
+    jobQueue.add("job", job);
 }));
 const saveCode = () => {
     // Implement code saving logic if needed
--- server/src/controller/code.ts
+++ server/src/controller/code.ts
@@ -2,74 +2,26 @@ import { Response, Request } from "express";
 
 import { ApiError } from "../utils/apiError";
 import { ApiResponse } from "../utils/apiResponse";
-
-import Docker from 'dockerode';
 import { asyncHandler } from "../utils/asyncHandler";
 import Job from "../model/job";
+import {Queue} from 'bullmq';
+
+const jobQueue = new Queue("jobQueue",{
+    connection:{
+        host:"0.0.0.0",
+        port:6379
+    }
+});
 
 export const executeCode = asyncHandler(async (req: Request, res: Response) => {
-    const docker = new Docker();
     const { code, userId, language } = req.body;
-
     if (!code || !language) {
         throw new ApiError(400, "Code and language are required");
     }
     const job = await Job.create({code,language,userId});
     res.status(200).json({jobId:job._id});
-    let image;
-    let command;
-
-    // Define Docker configurations based on language
-    switch (language.toLowerCase()) {
-        case 'javascript':
-            image = 'node';
-            command = ['node', '-e', code];
-            break;
-        case 'java':
-            image = 'openjdk';
-            command = ['bash', '-c', `echo '${code}' > Main.java && javac Main.java && java Main`];
-            break;
-        case 'cpp':
-            image = 'gcc';
-            command = ['bash', '-c', `echo '${code}' > main.cpp && g++ main.cpp -o main && ./main`];
-            break;
-        case 'python':
-            image = 'python:latest';
-            command = ['bash', '-c', `echo '${code}' > script.py && python script.py`];
-            break;
-        case 'c':
-            image = 'gcc';
-            command = ['bash', '-c', `echo '${code}' > main.c && gcc main.c -o main && ./main`];
-            break;
-        default:
-            throw new ApiError(400, "Unsupported language");
-    }
-
-    const containerConfig = {
-        Image: image,
-        Tty: false,
-        AttachStdout: true,
-        AttachStderr: true,
-        Cmd: command,
-    };
-    try {
-        job.startedAt = new Date();
-        const container = await docker.createContainer(containerConfig);
-        await container.start();
-        await container.wait();
-        const containerLogs = await container.logs({ stdout: true, stderr: true });
-        const containerResult = containerLogs.toString('utf-8').trim();
-        console.log(containerResult);
-        job["completedAt"] = new Date();
-        job["status"] = "success";
-        job["output"] = containerResult;
-        await job.save();
-    } catch (error:any) { 
-        job["completedAt"] = new Date();
-        job["output"] = error.message;
-        job["status"] = "failed";
-        throw new ApiError(500,JSON.stringify(error.message));
-    }
+    console.log("Adding job in queue");
+    jobQueue.add("job",job);
 });
 
 export const saveCode = () => {
