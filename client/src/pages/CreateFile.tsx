bff84fbbe1f69a65d3f0c96edd8d2b26d8cadd8b
pradeepkundekar <pradeepkundekargithub@gmail.com>
2024-02-12 23:54:10 +0530

    created sockets directory and index.js

--- client/src/pages/CreateFile.tsx
+++ client/src/pages/CreateFile.tsx
@@ -1,8 +1,7 @@
-
 import { useState } from 'react'
 import { useAppSelector } from '../app/hooks';
 import { notify } from '../utils/notify';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import useAxios from '../hooks/useAxios';
 import { Toaster } from 'react-hot-toast';
 const CreateFile = () => {
@@ -39,7 +38,10 @@ const CreateFile = () => {
 
                 <input 
                   type="submit" value="Let's Go" className='inline-flex items-center w-full px-5 py-3 mb-3 mr-1 text-base font-semibold text-white no-underline align-middle bg-blue-600 border border-transparent border-solid rounded-md cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-blue-700 hover:border-blue-700 hover:text-white focus-within:bg-blue-700 focus-within:border-blue-700'/>
+           
             </form>
+            <Link className='text-white' to={"/"}>Back</Link>
+            
         </div>
     </div>
   )
