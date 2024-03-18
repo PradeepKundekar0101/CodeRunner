0764755318b09bcccdbf518922fc97ee028337fe
Pradeep Kundekar <pradeepkundekar@Pradeeps-MacBook-Air.local>
2024-03-18 21:00:59 +0530

    fixed navbar logo

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -45,11 +45,7 @@ export default function Example() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src={"logo.png"}
-                    alt="CoderBro"
-                  />
+                 <h1 className="text-sm text-bold text-[#6366F1]">CoderBro</h1>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
